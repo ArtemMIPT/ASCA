24026aa4c4cbcf105e626d421af899e14c12c1ea
root <potapovart16@yandex.ru>
2016-11-21 16:01:37 +0300

    Added test project

---
+++ mapped_file/CMakeFiles/mappedfile.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mappedfile.dir/chunkmanager.c.o"
+  "CMakeFiles/mappedfile.dir/hash_table.c.o"
+  "CMakeFiles/mappedfile.dir/lists.c.o"
+  "CMakeFiles/mappedfile.dir/logger.c.o"
+  "CMakeFiles/mappedfile.dir/mapped_file.c.o"
+  "out/libmappedfile.pdb"
+  "out/libmappedfile.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mappedfile.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
